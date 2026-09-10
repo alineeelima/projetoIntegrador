089f22e79e07a7d9359bbf019db983824fa77c4c
53602007820 <aline810delima@gmail.com>
2026-09-10 16:09:53 -0300

    Feat: Desafio integrador

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,77 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite o nome do colaborador: ");
+        String nome = entrada.nextLine();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        System.out.print("Digite a idade do funcionário: ");
+        int idade = entrada.nextInt();
+
+        System.out.print("Digite o salário mensal do colaborador: ");
+        double salario = entrada.nextDouble();
+
+        System.out.print("Digite o tempo de empresa desse funcionário (em meses): ");
+        int tempoEmpresa = entrada.nextInt();
+
+        System.out.print("Digite a quantidade de filhos desse colaborador: ");
+        int quantFilhos = entrada.nextInt();
+
+        System.out.print("Digite a modalidade de trabalho desse funcionário (presencial ou home office): ");
+        String modalidade = entrada.nextLine();
+
+        System.out.print("O colaborador possui veículo próprio? (sim/não) ");
+        String veiculo = entrada.nextLine();
+
+        /// O bônus do PLR é a mesma quantidade do salário
+        double plr = salario;
+
+        ///  funcionários com salários abaixo de R$4.000,00 tem direito ao vale alimentação
+        if (salario <= 4000) {
+            System.out.println("A(o) funcionária(o)" + nome + "possui direito ao vale alimentação;");
+        }else {
+            System.out.println("O funcionário " + nome + " não possui direito ao vale alimentação;");
         }
+
+        /// Funcionários com pelo menos um filho tem direito ao auxílio creche
+        if (quantFilhos > 0) {
+            System.out.println("Possui direito ao auxílio creche;");
+        }else {
+            System.out.println("Não possui direito ao auxílio creche;");
+        }
+
+        /// Funcionários com mais de três meses de contratação tem direito ao plano de saúde
+        if (tempoEmpresa > 3) {
+            System.out.println("Pode aderir ao plano de saúde;");
+        }else {
+            System.out.println("Não pode aderir ao plano de saúde;");
+        }
+
+        /// Funcionários que trabalham no modelo Home Office possuem direito ao auxílio Home Office
+        if (modalidade.equalsIgnoreCase("HOME OFFICE")) {
+            System.out.println("Possui direito ao auxílio Home Office;");
+        }else {
+            System.out.println(" Não possui direito ao auxílio Home Office;");
+        }
+
+        /// Funcionários que possuem veículo próprio tem direito ao auxílio combustível
+        if (veiculo.equalsIgnoreCase("SIM")) {
+            System.out.println("Possui direito ao auxílio combustível;");
+        }else {
+            System.out.println("Não possui direito ao auxílio combustível;");
+        }
+
+        ///  funcionários com mai de 1 ano de empresa tem direito a bolsa de estudos
+        if (tempoEmpresa >=12) {
+            System.out.println("Possui direito a bolsa de estudos;");
+        }else {
+            System.out.println("Não possui direito a bolsa de estudos;");
+
+        }
+
+        System.out.println("E vai receber R$" + plr + "de participação nos lucros da empresa.");
     }
 }
